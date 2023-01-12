332ac6ac5515c9859d33261beddfe0ab9a80af0f
Raj Thombare <therajthombare@gmail.com>
2023-01-12 23:35:21 +0530

    add asset & font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import logo from "./assets/github-logo.png";
+
 function App() {
   return (
-    <div>
-      Github Finder
+    <div className="my-2 mx-20 p-2 flex">
+      <div>
+        <h1 className="text-6xl font-inter font-bold">Github Finder</h1>
+        <h3 className="text-xl font-normal">
+          Your site to find programmers quickly and easily!
+        </h3>
+      </div>
+      <div className="h-100">
+        <img className="h-full" src={logo} alt="github logo" />
+      </div>
     </div>
   );
 }
