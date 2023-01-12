9dda788714713a595ec3111b64feaeb58ac11265
Raj Thombare <therajthombare@gmail.com>
2023-01-12 23:52:34 +0530

    landing page done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import logo from "./assets/github-logo.png";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
-    <div className="my-2 mx-20 p-2 flex">
-      <div>
-        <h1 className="text-6xl font-inter font-bold">Github Finder</h1>
-        <h3 className="text-xl font-normal">
-          Your site to find programmers quickly and easily!
-        </h3>
-      </div>
-      <div className="h-100">
-        <img className="h-full" src={logo} alt="github logo" />
-      </div>
-    </div>
+    <>
+      <LandingPage />
+    </>
   );
 }
 
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,18 @@
+import logo from "../assets/github-logo.png";
+const LandingPage = () => {
+  return (
+    <div className="my-2 mx-40 p-2 flex items-center justify-between">
+      <div className="text-left p-1">
+        <h1 className="text-7xl font-inter font-bold mb-4">GitHub Finder</h1>
+        <h3 className="text-2xl font-normal">
+          Your site to find programmers quickly and easily!
+        </h3>
+      </div>
+      <div className="h-80">
+        <img className="h-full" src={logo} alt="github logo" />
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
