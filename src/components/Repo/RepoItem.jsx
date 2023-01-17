f4d674277111acea7fd4a706472c7501ac17d0f6
Raj Thombare <therajthombare@gmail.com>
2023-01-17 20:48:41 +0530

    style User page

--- src/components/Repo/RepoItem.jsx
+++ src/components/Repo/RepoItem.jsx
@@ -1,28 +1,28 @@
 const RepoItem = ({ repoData }) => {
   const { url, name, description, language, stars, forks, watchers } = repoData;
   return (
-    <div className="p-7 m-4 bg-[#e5e7eb] rounded-xl shadow">
+    <div className="p-7 m-4 bg-[#f0f0f0] rounded-xl shadow">
       <div className="flex items-baseline justify-between mb-3">
         <div>
           <a href={url} className="font-semibold text-xl">
             {name}
           </a>
-          <p className="font-normal text-base py-1">{description}</p>
-          <h3 className="font-normal text-base py-1">{language}</h3>
+          <p className="font-light text-base py-1">{description}</p>
+          <h3 className="font-light text-base py-1">{language}</h3>
         </div>
         <div>
           <a
             href={url}
             target="_blank"
             rel="noreferrer"
-            className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold text-sm rounded-xl"
+            className="px-4 py-2 bg-black hover:opacity-75 text-white font-semibold text-sm rounded-xl"
           >
             Github
           </a>
         </div>
       </div>
       <div>
-        <div className="mt-2 flex flex-row justify-between w-full font-medium text-sm gap-x-2">
+        <div className="mt-2 flex flex-row justify-between w-full font-light text-sm gap-x-2">
           <div>{stars} Stars</div>
           <div>{forks} Forks</div>
           <div>{watchers} Watching</div>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -66,35 +66,38 @@ const User = () => {
               src={user?.avatar}
               width="200px"
               alt="avatar"
-              className="rounded-xl mb-5"
+              className="rounded-xl mb-3"
             />
-            <div className="flex flex-row justify-between my-2 w-full font-semibold text-lg">
-              <div>Follower: {user?.follower}</div>
-              <div>Following: {user?.following}</div>
-            </div>
+            <dl class="grid max-w-screen-xl grid-cols-2 gap-x-2 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-3 dark:text-white items-baseline">
+              <div class="flex flex-col items-center justify-center">
+                <dt class="mb-2 text-2xl font-extrabold">{user?.follower}</dt>
+                <dd class="font-light text-gray-500 dark:text-gray-400">
+                  Follower
+                </dd>
+              </div>
+              <div class="flex flex-col items-center justify-center">
+                <dt class="mb-2 text-2xl font-extrabold">{user?.following}</dt>
+                <dd class="font-light text-gray-500 dark:text-gray-400">
+                  Following
+                </dd>
+              </div>
+              <div class="flex flex-col items-center justify-center">
+                <dt class="mb-2 text-2xl font-extrabold">
+                  {user?.public_repos}
+                </dt>
+                <div class="font-light text-gray-500 dark:text-gray-400">
+                  Repository
+                </div>
+              </div>
+            </dl>
             <h2 className="font-bold text-2xl">{user?.name}</h2>
             <h1 className="font-semibold text-lg">@{user?.username}</h1>
-            <div className="font-semibold text-lg">
-              Works at {user?.company}
-            </div>
-            <div className="font-semibold text-lg">
-              Location: {user?.location}
-            </div>
-            <a
-              href={`https://twitter.com/${user?.twitter}`}
-              target="_blank"
-              rel="noreferrer"
-              className="font-semibold text-lg"
-            >
-              Twitter: {user?.twitter}
-            </a>
-            <div className="font-semibold text-lg">
-              Public Repository: {user?.public_repos}
-            </div>
+            <div className="text-normal">Works at {user?.company}</div>
+            <div className="text-normal">Location: {user?.location}</div>
             <a
               type="button"
               href={user?.github_url}
-              class="mt-3 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+              class="mt-3 text-white bg-black hover:opacity-75 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
             >
               Visit Profile
             </a>
