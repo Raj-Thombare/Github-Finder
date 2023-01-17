a5d8e30b0eb161d728f278e0d863b8e8ada0eed3
Raj Thombare <therajthombare@gmail.com>
2023-01-17 20:11:09 +0530

    style and fix responsivenss

--- src/components/Repo/RepoItem.jsx
+++ src/components/Repo/RepoItem.jsx
@@ -1,7 +1,7 @@
 const RepoItem = ({ repoData }) => {
   const { url, name, description, language, stars, forks, watchers } = repoData;
   return (
-    <div className="p-7 m-4 bg-[#e5e7eb] rounded-xl">
+    <div className="p-7 m-4 bg-[#e5e7eb] rounded-xl shadow">
       <div className="flex items-baseline justify-between mb-3">
         <div>
           <a href={url} className="font-semibold text-xl">
@@ -15,14 +15,14 @@ const RepoItem = ({ repoData }) => {
             href={url}
             target="_blank"
             rel="noreferrer"
-            className="px-4 py-2 bg-[#2563eb] text-white font-semibold text-sm rounded-xl"
+            className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold text-sm rounded-xl"
           >
             Github
           </a>
         </div>
       </div>
       <div>
-        <div className="mt-2 flex flex-row justify-between w-full font-medium text-sm opacity-75 gap-x-2">
+        <div className="mt-2 flex flex-row justify-between w-full font-medium text-sm gap-x-2">
           <div>{stars} Stars</div>
           <div>{forks} Forks</div>
           <div>{watchers} Watching</div>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -54,50 +54,55 @@ const User = () => {
   }, [login, repoUrl, getUserData, getUserRepos]);
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2">
-      <div className="p-5 flex flex-col justify-center min-w-[40%] min-h-screen left-0 md:fixed">
-        <div
-          className="card compact side bg-base-100 p-5 flex flex-col justify-center items-center border border-gray-200 rounded-lg shadow-md
+    <div>
+      <div className="text-4xl p-4 bg-white sticky top-0 shadow-lg">Navbar</div>
+      <div className="min-w-[100%] flex flex-col md:flex flex-row ">
+        <div className="p-5 flex flex-col justify-start min-w-[40%] min-h-fit md:fixed left-0 min-h-screen">
+          <div
+            className="card compact side bg-base-100 p-5 flex flex-col justify-center items-center border border-gray-200 rounded-lg shadow-md 
           dark:bg-gray-800 dark:border-gray-700"
-        >
-          <Image
-            src={user?.avatar}
-            width="200px"
-            alt="avatar"
-            className="rounded-xl mb-5"
-          />
-          <div className="flex flex-row justify-between my-2 w-full font-semibold text-lg">
-            <div>Follower: {user?.follower}</div>
-            <div>Following: {user?.following}</div>
-          </div>
-          <h2 className="font-bold text-2xl">{user?.name}</h2>
-          <h1 className="font-semibold text-lg">@{user?.username}</h1>
-          <div className="font-semibold text-lg">Works at {user?.company}</div>
-          <div className="font-semibold text-lg">
-            Location: {user?.location}
-          </div>
-          <a
-            href={`https://twitter.com/${user?.twitter}`}
-            target="_blank"
-            rel="noreferrer"
-            className="font-semibold text-lg"
           >
-            Twitter: {user?.twitter}
-          </a>
-          <div className="font-semibold text-lg">
-            Public Repository: {user?.public_repos}
+            <Image
+              src={user?.avatar}
+              width="200px"
+              alt="avatar"
+              className="rounded-xl mb-5"
+            />
+            <div className="flex flex-row justify-between my-2 w-full font-semibold text-lg">
+              <div>Follower: {user?.follower}</div>
+              <div>Following: {user?.following}</div>
+            </div>
+            <h2 className="font-bold text-2xl">{user?.name}</h2>
+            <h1 className="font-semibold text-lg">@{user?.username}</h1>
+            <div className="font-semibold text-lg">
+              Works at {user?.company}
+            </div>
+            <div className="font-semibold text-lg">
+              Location: {user?.location}
+            </div>
+            <a
+              href={`https://twitter.com/${user?.twitter}`}
+              target="_blank"
+              rel="noreferrer"
+              className="font-semibold text-lg"
+            >
+              Twitter: {user?.twitter}
+            </a>
+            <div className="font-semibold text-lg">
+              Public Repository: {user?.public_repos}
+            </div>
+            <a
+              type="button"
+              href={user?.github_url}
+              class="mt-3 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+            >
+              Visit Profile
+            </a>
           </div>
-          <a
-            type="button"
-            href={user?.github_url}
-            class="my-5 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
-          >
-            Visit Profile
-          </a>
         </div>
-      </div>
-      <div className="min-w-[60%] md:absolute right-0">
-        <RepoList />
+        <div className="min-w-[60%] ml-[0%] md:ml-[40%]">
+          <RepoList />
+        </div>
       </div>
     </div>
   );
